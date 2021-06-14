ea68548d6fc4eb731098c20d1f9399bd1c34da6a
Neeta Timsina <neetatimsina@Neetas-MacBook-Pro.local>
2021-06-14 15:07:45 +0545

    database initiated

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 CORS(app)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://xpzupniv:T9dyWSQc1cF3JRUCkSLYUU7j6aadQvsC@batyr.db.elephantsql.com/xpzupniv"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
 @app.route('/')
 def hello():
     return 'Polimeter Nepal !!'
+
